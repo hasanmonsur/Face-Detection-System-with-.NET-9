e5dde27fe23f079cd0eb064a35c54ada5fd0c421
MD HASAN MONSUR <hasanmcse@gmail.com>
2025-02-09 00:08:35 +0600

    hasan03

---
+++ Controllers/FaceDetectionController.cs
@@ -0,0 +1,29 @@
+﻿using FaceDetectionAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaceDetectionAPI.Controllers
+{
+    [ApiController]
+    [Route("api/face")]
+    public class FaceDetectionController : ControllerBase
+    {
+        private readonly FaceDetectionService _faceDetectionService;
+
+        public FaceDetectionController(FaceDetectionService faceDetectionService)
+        {
+            _faceDetectionService = faceDetectionService;
+        }
+
+        [HttpPost("detect")]
+        public IActionResult DetectFaces(IFormFile file)
+        {
+            var filePath = Path.GetTempFileName();
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            var faces = _faceDetectionService.DetectFaces(filePath);
+            return Ok(new { FacesDetected = faces.Count });
+        }
+    }
+}
